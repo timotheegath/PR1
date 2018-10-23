834c74377bd118a52dc5a61591f78f6ad7b753c7
Luka <luka.lagator15@imperial.ac.uk>
2018-10-23 21:15:41 +0100

    AHAHAHA

--- ex1.py
+++ ex1.py
@@ -4,16 +4,28 @@ import matplotlib.pyplot as plt
 import scipy.io
 from scipy.io import loadmat
 
+INPUT_PATH = 'data/face.mat'
+TRAINING_SPLIT = 0.7
 
 def import_processing(data):
 
     faces = loadmat(data)
-    X = np.reshape(faces['X'], (46, 56, 520))
-    X = X.transpose
-    return X
+    X = np.reshape(faces['X'], (46, 56, 10, 52))
+    X = X.transpose()
 
-def split_data(X)
+    X = split_data(X)
+    means = [np.mean(x, axis=2) for x in X]
+    data = [x - means[i][..., None] for i, x in enumerate(X)]
+    return data
 
 
+def split_data(X):
 
-X = import_processing('data/face.mat')
+    training_data = np.reshape(X[..., 0:int(TRAINING_SPLIT*10), :], (46, 56, -1))
+    test_data = np.reshape(X[..., int(TRAINING_SPLIT*10):, :], (46, 56, -1))
+    data = [training_data, test_data]
+
+    return data
+
+
+X = import_processing(INPUT_PATH)
