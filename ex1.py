7cea6c1efaf2352f1af34573a8534f56aa24b44d
timotheegath <timogath@mail.com>
2018-10-23 21:40:47 +0100

    Eigenvectors and values

--- ex1.py
+++ ex1.py
@@ -10,22 +10,47 @@ TRAINING_SPLIT = 0.7
 def import_processing(data):
 
     faces = loadmat(data)
-    X = np.reshape(faces['X'], (46, 56, 10, 52))
+    X = np.reshape(faces['X'], (46*56, 10, 52))
     X = X.transpose()
 
     X = split_data(X)
-    means = [np.mean(x, axis=2) for x in X]
-    data = [x - means[i][..., None] for i, x in enumerate(X)]
+    means = [np.mean(x, axis=1) for x in X]
+    data = [(x - means[i][..., None]) for i, x in enumerate(X)]
     return data
 
 
 def split_data(X):
 
-    training_data = np.reshape(X[..., 0:int(TRAINING_SPLIT*10), :], (46, 56, -1))
-    test_data = np.reshape(X[..., int(TRAINING_SPLIT*10):, :], (46, 56, -1))
+    training_data = np.reshape(X[..., 0:int(TRAINING_SPLIT*10), :], (46*56, -1))
+    test_data = np.reshape(X[..., int(TRAINING_SPLIT*10):, :], (46*56, -1))
     data = [training_data, test_data]
 
     return data
 
+def compute_S(data):
+
+    N = data.shape[1]
+    S = np.cov(data)/N
+    return S
+
+
+def find_eigenvectors(S, how_many=-1):
+
+    if how_many is -1:
+        how_many = S.shape[0]
+    eigvalues, eigvectors = np.linalg.eig(S)
+    indices = np.flip(np.argsort(eigvalues), axis=0) # Gives original indices after sorting
+    sorted_eigvalues = eigvalues[indices]
+    sorted_eigvectors = eigvectors[:, indices]
+
+    return (sorted_eigvalues[0:how_many], sorted_eigvectors[0:how_many])
+
+
+
+
 
 X = import_processing(INPUT_PATH)
+# On training data
+S = compute_S(X[0])
+eig = find_eigenvectors(S)
+print(eig)
