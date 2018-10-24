d52413a0a259cf2bfd664eccc1d3f9b1b981e036
Timo <tlg15@ic.ac.uk>
2018-10-24 13:47:12 +0100

    Added little stuff

--- display.py
+++ display.py
@@ -4,17 +4,16 @@ from scipy.io import loadmat
 import cv2
 
 
-
 def display_single_image(X):
 
     X = np.reshape(X, (56, 46))
     cv2.imshow('Single face', X)
     cv2.waitKey()
 
+
 def display_eigenvectors(vecs):
     # 10 images a column
     vecs = np.abs(vecs)
-
     how_many = vecs.shape[0]
     rows = how_many // 10
     pics = np.zeros((56*rows, 46*10), dtype=np.uint8)
@@ -28,3 +27,7 @@ def display_eigenvectors(vecs):
                 break
     cv2.imshow('Eigenvectors', pics)
     cv2.waitKey()
+
+    if __name__ is '__main__':
+
+        """Put things here that should launch if the this script is called directly but not if it's imported"""
