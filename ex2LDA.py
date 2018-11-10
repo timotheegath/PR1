56001a278149877496cab8af156e860b7acf0547
Luka <luka.lagator15@imperial.ac.uk>
2018-11-10 15:50:04 +0000

    Just maths from slides
    PCA-reduction, class means and scatter, and Sw and Sb computation

--- ex2LDA.py
+++ ex2LDA.py
@@ -68,7 +68,7 @@ def find_projection(eigenvectors, faces):  # eigenvectors and faces in vector fo
     return coeffs
 
 
-def Reduce_by_PCA(training_data):
+def reduce_by_PCA(training_data):
 
     low_S = compute_S(training_data, low_res=True)
     eig_val, eig_vec = find_eigenvectors(low_S, how_many=-1)
@@ -77,9 +77,44 @@ def Reduce_by_PCA(training_data):
     projection_coeffs = find_projection(eig_vec_reduced, training_data)
     return projection_coeffs
 
+
+def compute_class_means(training_data):
+
+    class_means = np.mean(training_data.reshape(-1, TRAINING_SPLIT, NUMBER_PEOPLE), axis=1) # Shape is 2576*52 -> D*c
+    return class_means
+
+
+def compute_class_scatters(training_data, class_means):
+
+    class_means = np.repeat(class_means, TRAINING_SPLIT, axis=1)
+    class_means = class_means.reshape(-1, TRAINING_SPLIT, NUMBER_PEOPLE).transpose(2, 0, 1)
+    training_data = training_data.reshape(-1, TRAINING_SPLIT, NUMBER_PEOPLE).transpose(2, 0, 1)
+    print(training_data.shape, class_means.shape)
+    class_scatters = np.matmul(training_data - class_means, (training_data - class_means).transpose(0, 2, 1))
+    # Might have to for loop but I think it works
+    print(class_scatters.shape)
+    return class_scatters
+
+# def compute_Sb(class_means):
+
+    # class_means - mean        -> Two classes -> class1_mean - class2_mean
+    #                           -> Multiple classes -> slides say mean_class - mean (what mean!?)
+    #                           -> Is it mean difference between each pair of classes (or between each class and total mean)
+
+
+def compute_Sw(class_scatters):
+
+    Sw = np.sum(class_scatters, axis=0)
+    print(Sw.shape)
+    return Sw
+
 if __name__ == '__main__':
 
     [training_data, testing_data], means = import_processing(INPUT_PATH)    # Training and Testing data have the
     # training mean removed
-    reduced_training_data = Reduce_by_PCA(training_data)
-    
+    reduced_training_data = reduce_by_PCA(training_data)
+    class_means = compute_class_means(training_data)
+    class_scatters = compute_class_scatters(training_data, class_means)
+    #Sb = compute_Sb(class_means)
+    Sw = compute_Sw(class_scatters)
+
