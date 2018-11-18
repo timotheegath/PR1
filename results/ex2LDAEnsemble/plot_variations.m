67b9a04ed5bfa6e10e9ba2a623d00a70ce950cb7
timotheegath <timogath@mail.com>
2018-11-18 23:27:33 +0000

    Update plot_variations.m

--- results/ex2LDAEnsemble/plot_variations.m
+++ results/ex2LDAEnsemble/plot_variations.m
@@ -1,6 +1,6 @@
 close all, clear all
 
-load('acc_time_varying_bag_size')
+load('acc_time_varying_bag_sizeM_LDA_is_true')
 plot(bag_size, accuracy)
 xlabel('Bag size')
 grid on
@@ -11,5 +11,14 @@ plot(bag_size, repeats_in_bag)
 xlabel('Bag size')
 grid on
 ylabel('Repeat percentage')
+
+figure()
+
+
+plot(M_LDA, accuracy, '*')
+xlabel('M_LDA')
+grid on
+ylabel('Accuracy')
+
 
 
