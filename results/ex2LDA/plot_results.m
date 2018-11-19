2fc85e25dbdeb4ca415fc44928dfee7cfd98b0cc
timotheegath <timogath@mail.com>
2018-11-19 22:50:03 +0000

    results

--- results/ex2LDA/plot_results.m
+++ results/ex2LDA/plot_results.m
@@ -3,21 +3,27 @@ M_LDAvary = load('split_7m_pca312VARY_M_LDA.mat');
 M_PCAvary = load('split_7m_lda51VARY_M_PCA2.mat');
 
 figure()
-title('When varying M_LDA')
-plot(M_LDAvary.M_LDA, M_LDAvary.accuracy)
-xlabel('M_(LDA) values')
-ylabel('Accuracy')
-set(gca, 'XDir','reverse')
-legend('With M_(PCA) = N - c')
-grid on
+title('Accuracy with varying values of M_{LDA} and M_{PCA}')
 
-figure()
-title('WHen varying M_PCA')
-plot(M_PCAvary.M_PCA, M_PCAvary.accuracy)
-xlabel('M_(PCA) values')
-ylabel('Accuracy')
+subplot(211)
+
 set(gca, 'XDir','reverse')
-legend('With M_(LDA) = c - 1')
+plot(M_LDAvary.M_LDA, M_LDAvary.accuracy*100)
+ylim([0 100])
+title('With M_{PCA} = N - c')
+xlabel('M_{LDA} values')
+ylabel('Accuracy (%)')
+set(gca, 'XDir','reverse')
+
+grid on
+subplot(212)
+
+plot(M_PCAvary.M_PCA, M_PCAvary.accuracy*100)
+ylim([0 100])
+xlabel('M_{PCA} values')
+ylabel('Accuracy (%)')
+set(gca, 'XDir','reverse')
+title('With M_{LDA} = c - 1')
 grid on
 
 
