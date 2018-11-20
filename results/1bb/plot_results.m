17ae669d853aac5f30469017390e5760099c0f68
timotheegath <timogath@mail.com>
2018-11-20 23:46:17 +0000

    results

--- results/1bb/plot_results.m
+++ results/1bb/plot_results.m
@@ -1,12 +1,12 @@
 clear all, close all
-load('results_NN_vs_eig_faces');
+load('results_REC_vs_eig_faces');
 
 
 figure()
-plot(n_eigenvecs, NN_accuracy * 100)
+%plot(n_eigenvecs, NN_accuracy * 100)
 grid on
 %hold on
-%plot(n_eigenvecs, REC_accuracy * 100)
+plot(n_eigenvecs, REC_accuracy * 100)
 
 xlabel('Number of eigenfaces used')
 ylabel('Accuracy (%)')
